c8f72e29b403adfefd87f9766696f5ab606084b3
Elijah Noble <elijahnobledev@gmail.com>
2022-06-16 23:41:38 -0700

    Assignment Completed

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Login from "./components/Login";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
